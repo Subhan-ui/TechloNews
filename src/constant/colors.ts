7b7d23da666e78fcb7e6ab7ac6ac4fd7ddf7bccf
Subhan-ui <subhanshoukat1908@gmail.com>
2024-08-06 19:54:11 +0500

    nothing

--- src/constant/colors.ts
+++
@@ -1,5 +0,0 @@
-export const red = '#C31815'
-export const gray = '#2a2a2a'
-export const lightblack = '#000000bf'
-export const blue = '#0E1E32'
-export const lightGray = '#F1F2F3'
--- src/constant/links.ts
+++
@@ -1,9 +0,0 @@
-export const searchURL = (search: string | null) => {
-  return `${import.meta.env.VITE_APP_SEARCH_URL}${search}&api-key=${import.meta.env.VITE_APP_API_KEY}`;
-};
-
-export const categoryURL:string = import.meta.env.VITE_APP_CATEGORY_URL
-
-export const sectionsURL = (category: string) => {
-  return `${import.meta.env.VITE_APP_SECTION_URL}${category}.json?limit=20&api-key=${import.meta.env.VITE_APP_API_KEY}`;
-};
